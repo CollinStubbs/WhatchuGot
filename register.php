e1b0bc7141c8908c1d287cf0dd7e2ff27cb5f10a
CollinStubbs <collin.stubbs@uoit.net>
2014-12-01 12:37:34 -0500

    Added bootstrap to Register.php

--- register.php
+++ register.php
@@ -49,53 +49,132 @@
 	}
 	else
 	{
-		$message = "Please Enter all the fields";
+		$message = "Fill out the information below to register with us!";
 	}
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-</head>
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Final Proj.</title>
+        <meta name="description" content="Final Project">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css.css">
+        
+        <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
 
-<body>
-<?php 
-	echo $message ;
-?>
-<form action="register.php" method="post" name="registerationFrom">
-Username<br />
-<input name="uName" type="text" /><br />
-Password<br />
-<input name="uPassword" type="password" /><br />
-First Name<br />
-<input name="fName" type="text" /><br />
-Last Name<br />
-<input name="lName" type="text" /><br />
-Email<br />
-<input name="uEmail" type="text" /><br />
-University<br />
- <select name="uUniversity" size="1">
- 	<option value="0"></option>
-    <option value="Brock">Brock</option>
-    <option value="Centennial">Centennial</option>
-    <option value="Durham College - Oshawa">Durham College - Oshawa</option>
-    <option value="Durham College - Whitby">Durham College - Whitby</option>
-    <option value="George Brown">George Brown</option>
-    <option value="McMaster">McMaster</option>
-    <option value="Queens">Queens</option>  
-    <option value="Ryerson">Ryerson</option>
-    <option value="Seneca">Seneca</option>
-    <option value="Trent University">Trent University</option>
-    <option value="UofT - Downtown">UofT - Downtown</option>
-    <option value="UofT - Scarborough">UofT - Scarborough</option>
-    <option value="UofT - St. George">UofT - St. George</option>
-    <option value="UOIT-Downtown">UOIT-Downtown</option>
- 	<option value="UOIT-North">UOIT-North</option>
-    <option value="Western University">Western University</option>
-    <option value="York University">York University</option>
- </select><br />
-<button name="submit" type="submit">Submit</button>
-</form>
-</body>
+        <!-- Custom CSS -->
+        <link href="css/agency.css" rel="stylesheet">
+
+        <!-- Custom Fonts -->
+        <link href="font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+        <link href='http://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+    </head>
+        <body>
+            <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand page-scroll" href="login.html">Whatchu Got?</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="register.html">Log-in</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="about.html">About</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="team.html">Team</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="contact.html">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+<!-- Header -->
+    <header class="aboutpage">
+        <div class="container">
+            <div class="contact-text">
+                <div class="body-lead-in">
+                <?php 
+                    echo $message ;
+                ?>
+                </div>
+                </div>
+               <form action="register.php" method="post" name="registerationFrom">
+                   <label><input type="text" class="form-control input-lg ContactInput" name="uName" placeholder="Username"></label><br><br>
+                   <label><input class="form-control input-lg ContactInput" type="text" name="fName" placeholder="First Name"></label><br><br>
+                   <label><input type="text" class="form-control input-lg ContactInput" name="lName" placeholder="Last Name"></label><br><br>
+                   
+                   <label><input type="text" class="form-control input-lg ContactInput" name="uEmail" placeholder="Email Address"></label><br><br>
+                   <label><input type="text" class="form-control input-lg ContactInput" name="uPassword" placeholder="Password"></label><br><br>                   
+                   
+                   
+                    <select name="uUniversity" style="color: #000;" size="1">
+                        <option value="0"></option>
+                        <option value="Brock">Brock</option>
+                        <option value="Centennial">Centennial</option>
+                        <option value="Durham College - Oshawa">Durham College - Oshawa</option>
+                        <option value="Durham College - Whitby">Durham College - Whitby</option>
+                        <option value="George Brown">George Brown</option>
+                        <option value="McMaster">McMaster</option>
+                        <option value="Queens">Queens</option>  
+                        <option value="Ryerson">Ryerson</option>
+                        <option value="Seneca">Seneca</option>
+                        <option value="Trent University">Trent University</option>
+                        <option value="UofT - Downtown">UofT - Downtown</option>
+                        <option value="UofT - Scarborough">UofT - Scarborough</option>
+                        <option value="UofT - St. George">UofT - St. George</option>
+                        <option value="UOIT-Downtown">UOIT-Downtown</option>
+                        <option value="UOIT-North">UOIT-North</option>
+                        <option value="Western University">Western University</option>
+                        <option value="York University">York University</option>
+                     </select><br /><br />
+                   
+                   <button type="submit" class="btn btn-lg btn-success" name="submit" style="background-color:#007C87">Submit</button>
+                </form>            
+        </div>
+    </header>
+               
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="js/classie.js"></script>
+    <script src="js/cbpAnimatedHeader.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/agency.js"></script>
+ </body>
 </html>
+
