b56ea58b06a6e2e99194af57b5138e38df3d9f36
CollinStubbs <collin.stubbs@uoit.net>
2014-12-01 12:10:07 -0500

    Bootrap working with login.php

--- version 2/login.php
+++ version 2/login.php
@@ -1,16 +1,121 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-</head>
 
-<body>
-<form action="signin.php" method="post">
-<label>Username<input type="text" name="loginName" value="" ></label><br><br>
-<label>Password<input type="password" name="pass" value=""></label><br><br>
-<button name="submit" type="submit">Login</button>
-<button name="signup" type="button" onclick="window.location='create.html';">Register</button>
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Final Proj.</title>
+        <meta name="description" content="Final Project">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css.css">
+        
+        <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom CSS -->
+        <link href="css/agency.css" rel="stylesheet">
+
+        <!-- Custom Fonts -->
+        <link href="font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+        <link href='http://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+    </head>
+        <body>
+            <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand page-scroll" href="login.html">Whatchu Got?</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="register.html">Log-in</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="about.html">About</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="team.html">Team</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="contact.html">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+<!-- Header -->
+    <header class="registerpage">
+        <div class="container">
+            <div class="logtext">
+                <div class="body-lead-in ">Login below!</div>
+                <div class="row" style="margin-top:20px">
+                <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
+                   <form action="signin.php" method="post">
+
+
+
+
+                        <fieldset>
+                            <hr class="colorgraph">
+                            <div class="form-group">
+                                <input type="text" name="loginName" id="email" class="form-control input-lg" placeholder="UserName" value="">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" name="pass" id="password" class="form-control input-lg" placeholder="Password" value="">
+                            </div>
+                            <hr class="colorgraph">
+                            <div class="row">
+                                <div class="col-xs-6 col-sm-6 col-md-6">
+                                    <button name="submit" class="btn btn-lg btn-success btn-block" type="submit" value="Login" style="background-color:#007C87">Login</button>
+                                   
+                                </div>
+                                <div class="col-xs-6 col-sm-6 col-md-6">
+                                    <button name="signup" class="btn btn-lg btn-success btn-block" value="Register" style="background-color: #fed136" type="button" onclick="window.location='create.html';">Register</button>
+                                  
+                                </div>
+                            </div>
+                        </fieldset>
 </form>
-</body>
+                </div>               
+            </div>
+        </div>
+    </header>
+               <!-- jQuery -->
+    
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="js/classie.js"></script>
+    <script src="js/cbpAnimatedHeader.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/agency.js"></script>
+ </body>
 </html>
+
